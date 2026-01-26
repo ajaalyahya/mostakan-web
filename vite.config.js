323e31c6ce5363329cdeb77776a265548db83e47
ajaalyahya <ayman.alyahya@gmail.com>
2026-01-26 21:31:34 +0300

    fix build error and remove imagemin

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import viteImagemin from 'vite-plugin-imagemin'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteImagemin({
-      webp: {
-        quality: 75,
-      },
-      mozjpeg: {
-        quality: 75,
-      },
-      pngquant: {
-        quality: [0.6, 0.8],
-      },
-    }),
-  ],
+  plugins: [react()],
 })
