00a5a749062301f6589fa8bd13f77f64244b748c
ajaalyahya <ayman.alyahya@gmail.com>
2026-01-26 13:27:45 +0300

    أضفت ScrollToTop وحافظت على Navbar فقط في الهوم

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Navbar from "./components/layout/Navbar";
 
+// ScrollToTop component
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 function Layout() {
   const location = useLocation();
 
   return (
     <>
+      <ScrollToTop /> {/* هنا فقط أضفنا التمرير لأعلى */}
+
       {/* النافبار يظهر فقط في الهوم */}
       {location.pathname === "/" && <Navbar />}
 
